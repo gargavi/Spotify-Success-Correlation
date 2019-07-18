93ae3efdd5752dd280231671eb5d85b7a46bcbb0
Avi Garg <avigarg2000@gmail.com>
2019-07-18 08:29:25 -0700

    Plotted with success on the y-axis

--- data_visual.py
+++ data_visual.py
@@ -17,17 +17,17 @@ for key in small_genre_dict.keys():
 
 #this section deals with plotting the total genre's in one entire section
 
-fig, axes  = plt.subplots(2, 1, figsize= (15.0, 10.0), sharex = True)
+fig, axes  = plt.subplots(1, 2, figsize= (20.0, 10.0), sharey = True)
 
 for title, df_list in pandas_dataframes.items():
-    axes[0].scatter(df_list[0]["success"], df_list[0]["lyrics"])
-    axes[1].scatter(df_list[0]["success"], df_list[0]["tempo"])
+    axes[0].scatter(df_list[0]["lyrics"], df_list[0]["success"])
+    axes[1].scatter(df_list[0]["tempo"], df_list[0]["success"])
 
 print(axes)
 input()
 fig.legend(labels = pandas_dataframes.keys())
-axes[0].title.set_text("Lyrics")
-axes[1].title.set_text("Tempo")
+axes[0].title.set_text("Tempo")
+axes[1].title.set_text("Lyrics")
 fig.suptitle("Genre Comparison")
 
 #next step is that we want to compare each genre to the artists complete inside of it
@@ -38,9 +38,9 @@ for genre, df_list in pandas_dataframes.items():
     y_coor = 0
     lyr_fig, lyr_axes = plt.subplots(4,4, figsize = (20.0, 10.0), sharex = True, sharey = True)
     suc_fig, suc_axes = plt.subplots(4, 4, figsize= (20.0, 10.0), sharex = True, sharey = True)
-    lyr_axes[x_coor][y_coor].scatter(df_list[0]["success"], df_list[0]["lyrics"])
+    lyr_axes[x_coor][y_coor].scatter(df_list[0]["lyrics"], df_list[0]["success"])
     lyr_axes[x_coor][y_coor].title.set_text("Total Genre")
-    suc_axes[x_coor][y_coor].scatter(df_list[0]["success"], df_list[0]["tempo"])
+    suc_axes[x_coor][y_coor].scatter(df_list[0]["tempo"], df_list[0]["success"])
     suc_axes[x_coor][y_coor].title.set_text("Total Genre")
     artist_database = df_list[1]
     for index, row in artist_database.iterrows():
@@ -54,12 +54,13 @@ for genre, df_list in pandas_dataframes.items():
             success_axes = suc_axes[x_coor][y_coor]
             temp_connect = alch.create_engine("mysql+pymysql://" + user + ':' + password + '@localhost/' + index)
             complete_artist = pd.read_sql_table("complete", temp_connect, index_col = "Song")
-            lyric_axes.scatter(complete_artist["success"], complete_artist["lyrics"])
-            success_axes.scatter(complete_artist["success"], complete_artist["tempo"])
+            lyric_axes.scatter(complete_artist["lyrics"], complete_artist["success"])
+            success_axes.scatter(complete_artist["tempo"], complete_artist["success"])
         except Exception as e:
             print(index + " failed because " + str(e))
         lyric_axes.title.set_text(index.replace("_", " "))
         success_axes.title.set_text(index.replace("_", " "))
+    
     lyr_fig.suptitle("Lyrical Comparsion for " + genre)
     suc_fig.suptitle("Tempo Comparsion for " + genre)
 
