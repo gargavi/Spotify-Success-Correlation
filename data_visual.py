61d8973e2256df854e779e2c055a30031d0d999d
Avi Garg <avigarg2000@gmail.com>
2019-07-18 08:23:21 -0700

    Plotting Success on the X Axis

--- data_visual.py
+++ data_visual.py
@@ -23,11 +23,45 @@ for title, df_list in pandas_dataframes.items():
     axes[0].scatter(df_list[0]["success"], df_list[0]["lyrics"])
     axes[1].scatter(df_list[0]["success"], df_list[0]["tempo"])
 
-
+print(axes)
+input()
 fig.legend(labels = pandas_dataframes.keys())
+axes[0].title.set_text("Lyrics")
+axes[1].title.set_text("Tempo")
+fig.suptitle("Genre Comparison")
 
+#next step is that we want to compare each genre to the artists complete inside of it
+    #theoretically should be a 4 X 4 figure with 15 artist plots and 1 genre plot
 
-
+for genre, df_list in pandas_dataframes.items():
+    x_coor = 0
+    y_coor = 0
+    lyr_fig, lyr_axes = plt.subplots(4,4, figsize = (20.0, 10.0), sharex = True, sharey = True)
+    suc_fig, suc_axes = plt.subplots(4, 4, figsize= (20.0, 10.0), sharex = True, sharey = True)
+    lyr_axes[x_coor][y_coor].scatter(df_list[0]["success"], df_list[0]["lyrics"])
+    lyr_axes[x_coor][y_coor].title.set_text("Total Genre")
+    suc_axes[x_coor][y_coor].scatter(df_list[0]["success"], df_list[0]["tempo"])
+    suc_axes[x_coor][y_coor].title.set_text("Total Genre")
+    artist_database = df_list[1]
+    for index, row in artist_database.iterrows():
+        if x_coor < 3:
+            x_coor += 1
+        else:
+            y_coor += 1
+            x_coor = 0
+        try:
+            lyric_axes = lyr_axes[x_coor][y_coor]
+            success_axes = suc_axes[x_coor][y_coor]
+            temp_connect = alch.create_engine("mysql+pymysql://" + user + ':' + password + '@localhost/' + index)
+            complete_artist = pd.read_sql_table("complete", temp_connect, index_col = "Song")
+            lyric_axes.scatter(complete_artist["success"], complete_artist["lyrics"])
+            success_axes.scatter(complete_artist["success"], complete_artist["tempo"])
+        except Exception as e:
+            print(index + " failed because " + str(e))
+        lyric_axes.title.set_text(index.replace("_", " "))
+        success_axes.title.set_text(index.replace("_", " "))
+    lyr_fig.suptitle("Lyrical Comparsion for " + genre)
+    suc_fig.suptitle("Tempo Comparsion for " + genre)
 
 
 plt.show()
